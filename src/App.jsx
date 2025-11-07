bfb136e8e1c8c365cad610526183c8499a6c16ec
Gaelle <gaelle.madelaine@gmail.com>
2025-11-07 12:44:04 +0100

    tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Hero from "./components/Hero"
+
+function App() {
+  
+
+  return (
+    <div id="app-container" class=" container mx-auto pt-5 min-h-screen  flex flex-col">
+      <Hero/>
+      <h1>Salut les morues</h1>
+    </div>
+  ) 
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section>
+      
+       <img src="/images/text hero.svg" alt="" /> 
+       </section>
+  )
+}
+
+export default Hero
